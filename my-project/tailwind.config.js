f17746f3d463e9fa14c16c149edbbbdbdf89cc8c
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-01 12:57:42 +0200

    fix: layout3

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,13 +1,33 @@
-/** @type {import('tailwindcss').Config} */ module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+// /** @type {import('tailwindcss').Config} */ module.exports = {
+//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+//   theme: {
+//     screens: {
+//       sm: "576px",
+//       // => @media (min-width: 576px) { ... }
+//     },
+//   },
+// };
+
+module.exports = {
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
     screens: {
       sm: "576px",
       // => @media (min-width: 576px) { ... }
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
     },
+    extend: {},
   },
+  plugins: [],
 };
